0d606d69deab55ea204f4fbcc2e5f92d5d681ec6
MaxiCredit <48129633+MaxiCredit@users.noreply.github.com>
2020-03-23 16:47:47 +0100

    Update new_acc.js

--- new_acc.js
+++ new_acc.js
@@ -1,15 +1,11 @@
-const lib = require("./lib");
 const {Client, Ed25519PrivateKey, AccountCreateTransaction} = require("@hashgraph/sdk");
 const BigNumber = require("bignumber.js");
-
-const TestUsers = "0.0.168447";
-const TestCredit = "0.0.169081";
 
 const client = new Client({
     network: {"0.testnet.hedera.com:50211": "0.0.3"},
     operator: {
-        account: "0.0.32593",
-        privateKey: "302e020100300506032b657004220420b567e9ab212f88ef11cabfed7bdcbe18c679a3866ce9e7583c6589d6a4b8b41f"
+        account: "...",
+        privateKey: "..."
     }
 });
 
